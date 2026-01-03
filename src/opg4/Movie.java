b8124c942a61554bb2dbbd83d42e64555571164a
Gabbyskii <gkadiga1@gmail.com>
2026-01-03 18:09:16 +0100

    Add director field and update Movie constructor
    Added director field and updated constructor to initialize it.

--- src/opg4/Movie.java
+++ src/opg4/Movie.java
@@ -3,19 +3,23 @@ package opg4;
 import java.util.List;
 
 public class Movie extends Media implements Playable {
-    private String director;
-    private int duration; // i minutter
 
+    private String director;
+    private int duration;
+
+    // Constructor der initialiserer film med titel, skuespillere, instruktør og varighed
     public Movie(String title, List<String> actors, String director, int duration) {
-        super(title, actors);
+        super(title, actors); // Kalder parent class constructor
         this.director = director;
         this.duration = duration;
     }
+
 
     public String getDirector() {
         return director;
     }
 
+    // Implementerer play() fra Playable - afspiller filmen
     @Override
     public void play() {
         System.out.println("Now playing movie: " + title);
@@ -23,11 +27,13 @@ public class Movie extends Media implements Playable {
         System.out.println("Duration: " + duration + " minutes");
     }
 
+    // Implementerer getDuration() fra Playable - returnerer filmens varighed
     @Override
     public int getDuration() {
         return duration;
     }
 
+    // Returnerer en formateret string med filmens information
     @Override
     public String toString() {
         return "Movie: " + title + " (Director: " + director + ", Duration: " + duration + " min)";
