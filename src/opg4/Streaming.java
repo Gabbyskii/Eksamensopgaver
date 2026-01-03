11d4aa4ff3f13997c3f669bdd3a8c727e154e255
Gabbyskii <gkadiga1@gmail.com>
2026-01-03 18:10:17 +0100

    Update Streaming.java

--- src/opg4/Streaming.java
+++ src/opg4/Streaming.java
@@ -1,34 +1,42 @@
 package opg4;
 
-// Streaming.java - Main system class
 import java.util.ArrayList;
 import java.util.List;
 
 public class Streaming {
+
     private List<Media> mediaLibrary;
 
     public Streaming() {
         this.mediaLibrary = new ArrayList<>();
     }
 
+    // Tilføjer medie (film eller serie) til biblioteket
     public void addMedia(Media media) {
         mediaLibrary.add(media);
     }
 
+    // Finder og returnerer en film baseret på titel (ignorerer store/små bogstaver)
     public Movie findMovie(String title) {
+
         for (Media media : mediaLibrary) {
+            // Tjekker om mediet er en film og om titlen matcher
             if (media instanceof Movie && media.getTitle().equalsIgnoreCase(title)) {
-                return (Movie) media;
+                return (Movie) media; // Caster til Movie og returnerer
             }
         }
         return null;
     }
 
+    // Finder og returnerer en episode baseret på serie-titel, sæson- og episode-nummer
     public Episode findEpisode(String seriesTitle, int seasonNumber, int episodeNumber) {
+        // Gennemgår alle medier i biblioteket
         for (Media media : mediaLibrary) {
+            // Tjekker om mediet er en serie
             if (media instanceof Series) {
                 Series series = (Series) media;
-                if (series.getTitle().equalsIgnoreCase(seriesTitle)) {
+
+                if (series.getTitle().equalsIgnoreCase(seriesTitle)) { // Tjekker om serie-titlen matcher
                     return series.getEpisode(seasonNumber, episodeNumber);
                 }
             }
@@ -36,6 +44,7 @@ public class Streaming {
         return null;
     }
 
+    // Afspiller en film baseret på titel
     public void playMovie(String title) {
         Movie movie = findMovie(title);
         if (movie != null) {
@@ -45,6 +54,7 @@ public class Streaming {
         }
     }
 
+    // Afspiller en episode baseret på serie-titel, sæson- og episode-nummer
     public void playEpisode(String seriesTitle, int seasonNumber, int episodeNumber) {
         Episode episode = findEpisode(seriesTitle, seasonNumber, episodeNumber);
         if (episode != null) {
@@ -55,16 +65,21 @@ public class Streaming {
         }
     }
 
+    // Printer hele mediebiblioteket med alle detaljer
     public void printLibrary() {
         System.out.println("=== Media Library ===");
+
         for (Media media : mediaLibrary) {
-            System.out.println(media);
-            if (media instanceof Series) {
+            System.out.println(media); // Printer medie-information
+
+            if (media instanceof Series) {  // Hvis mediet er serie, print alle sæsoner og episoder
                 Series series = (Series) media;
-                for (Season ss : series.getSeasons()) {
-                    System.out.println("  " + ss);
-                    for (Episode ep  : ss.getEpisodes()) {
-                        System.out.println("    " + ep);
+
+                for (Season ss : series.getSeasons()) { // Gennemgår alle sæsoner i serien
+                    System.out.println("  " + ss); // Printer sæson med indrykning
+
+                    for (Episode ep  : ss.getEpisodes()) { // Gennemgår alle episoder i sæsonen
+                        System.out.println("    " + ep); // Printer episode med større indrykning
                     }
                 }
             }
